a70928873ab84cbac2ca558d4f48de20f96972e1
lionneclement <lionneclement@gmail.com>
2020-06-04 13:58:22 +0200

    install react-router-dom && delete some file

--- front/src/components/Root.js
+++ front/src/components/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App() {
+function Root() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,4 +20,4 @@ function App() {
   );
 }
 
-export default App;
+export default Root;
